b179023a52802fad9a78326b0c0a08534c855c43
Andy Chen <andychen.105@gmail.com>
2021-08-28 22:00:13 -0400

    Got javascript code to perform calculation correctly and to display the correct numbers together when evaluating the operations. Also got the code to work with if stringing multiple numbers

--- app.js
+++ app.js
@@ -1,65 +1,160 @@
+let firstNum = '';
+let secondNum = '';
+let currentNum = '';
+let operand = '';
+let solution = '';
+
+const display = document.querySelector('.display');
+
+//function operate that takes an operator and 2 numbers and then calls one of the above functions on the number
+
 //function to add
 function toAdd (num1,num2){
-    console.log(num1+num2);
-    // return num1 + num2;
+    return solution = (num1+num2);
 }
-
-
 
 //function to subtract
 function toMinus (num1,num2){
-    console.log(num1-num2);
-    // return num1 - num2;
+    return solution = (num1-num2);
     
 }
 
-
-
-
 //function to multiply
 function toMultiply (num1,num2){
-    console.log(num1*num2);
-    // return num1*num2;
+    return solution = (num1*num2);
 }
-
-
 
 //function to divide
 
 function toDivide (num1,num2){
-   console.log(num1/num2);
-    // return num1 / num2;
+   return solution = (num1/num2);
 }
 
 
-
-//function operate that takes an operator and 2 numbers and then calls one of the above functions on the number
-
-function operate (operator, num1, num2) {
+function operateNum (operator, num1, num2) {
     switch (operator){
         case '+':
             toAdd(num1,num2);
+            display.textContent = solution;
+            firstNum = solution;
+            secondNum = '';
             break;
         case '-':
             toMinus(num1,num2);
+            display.textContent = solution;
+            firstNum = solution;
+            secondNum = '';
             break;
         case '*':
             toMultiply(num1,num2);
+            display.textContent = solution;
+            firstNum = solution;
+            secondNum = '';
             break;
         case '/':
             toDivide(num1,num2);
+            display.textContent = solution;
+            firstNum = solution;
+            secondNum = '';
             break;
     }
 }
 
-operate('*',2,2);
+
+function toClear() {
+    firstNum = '';
+    secondNum = '';
+    operand = '';
+    solution = '';
+    display.textContent = 0;
+}
 
 
-//function that populate the display when you click number buttons and storing the display value in a variable somewhere for use later
-
-//probably define a global variable that is passed something in this function
 
 
+function toEqual(){
+    let num1 = parseFloat(firstNum);
+    let num2 = parseFloat(secondNum);
+    operateNum(operand,num1,num2);
+    
+
+}
+//clear function 
+
+
+
+
+function selectNumber (value) {
+    if (value === '.' && firstNum.includes('.') && secondNum.includes('.')) return
+  
+    if (operand == '' && secondNum == ''){
+        firstNum = firstNum + value;
+        currentNum = firstNum;
+    }
+    
+    
+
+    if (operand != '' && firstNum != '') {
+        secondNum = secondNum + value;
+        currentNum = secondNum;
+    }
+    
+ 
+    
+
+
+}
+
+
+
+
+function displayValue () {    
+    display.textContent = currentNum;
+
+
+
+    }
+
+function selectOperator(value) {
+    
+    operand = value;
+    
+    if (operand != '' && firstNum != '' && secondNum != ''){
+        toEqual(value);
+    }
+        
+    
+
+}
 //addeventlisteners for each button clicked
+//calls displayvalue 
 
-//probably store the numbers in an object
+const numbers = document.querySelectorAll('.digits');
+    numbers.forEach((number) => {
+        number.addEventListener('click', () => {
+            selectNumber(number.textContent);
+            displayValue();
+        });
+}); 
+
+//addeventlistener for operators 
+const operators = document.querySelectorAll('.operators');
+operators.forEach((operate) => {
+    operate.addEventListener('click', () => { 
+        selectOperator(operate.textContent)
+        currentNum = '';
+    });
+});
+
+const equal = document.querySelector('.equal');
+equal.addEventListener('click', () => toEqual());
+
+// --------------------// --------------------// --------------------// --------------------// --------------------
+
+
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => toClear());
+
+// const decimal = document.querySelector('.decimal');
+// decimal.addEventListener('click', () => selectNumber(decimal.textContent));
