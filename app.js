680bd9d0b6c218f426189b3f089e84d65e186948
Andy Chen <andychen.105@gmail.com>
2021-08-30 15:06:56 -0400

    Finished calculator project. Added additional functionality to calculator with +/- sign and .. Added more styling to calculator.

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
+//variables
 let firstNum = '';
 let secondNum = '';
 let currentNum = '';
-let operand = '';
+let firstOperand = '';
 let solution = '';
-
 const display = document.querySelector('.display');
+display.textContent = 0;
 
-//function operate that takes an operator and 2 numbers and then calls one of the above functions on the number
 
 //function to add
 function toAdd (num1,num2){
-    return solution = (num1+num2);
+    return solution = Math.round((num1+num2)*1000)/1000;
 }
 
 //function to subtract
 function toMinus (num1,num2){
-    return solution = (num1-num2);
-    
+    return solution = Math.round((num1-num2)*1000)/1000;   
 }
 
 //function to multiply
 function toMultiply (num1,num2){
-    return solution = (num1*num2);
+    return solution = Math.round((num1*num2)*1000)/1000;
 }
 
 //function to divide
-
 function toDivide (num1,num2){
-   return solution = (num1/num2);
+   return solution = Math.round((num1/num2)*1000)/1000;
 }
 
-
+//selecting which operator to use based on user input and spits out solution
 function operateNum (operator, num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) return
     switch (operator){
         case '+':
             toAdd(num1,num2);
@@ -52,83 +51,99 @@ function operateNum (operator, num1, num2) {
             secondNum = '';
             break;
         case '/':
+            if (num2 === 0) {
+                display.textContent = `You funny`;
+                firstNum = null;
+                secondNum = null;
+            } 
+            else { 
             toDivide(num1,num2);
             display.textContent = solution;
             firstNum = solution;
-            secondNum = '';
+            secondNum = ''; }
             break;
     }
 }
 
-
+//clear function 
 function toClear() {
     firstNum = '';
     secondNum = '';
-    operand = '';
+    firstOperand = '';
     solution = '';
     display.textContent = 0;
 }
 
-
-
-
+//evaluating function that calls on the operating function to select which operator to perform
 function toEqual(){
     let num1 = parseFloat(firstNum);
     let num2 = parseFloat(secondNum);
-    operateNum(operand,num1,num2);
-    
-
+    operateNum(firstOperand,num1,num2);
 }
-//clear function 
 
-
-
-
+//user selects number
 function selectNumber (value) {
-    if (value === '.' && firstNum.includes('.') && secondNum.includes('.')) return
-  
-    if (operand == '' && secondNum == ''){
-        firstNum = firstNum + value;
-        currentNum = firstNum;
+    if (display.textContent == 0 && value == '.') {
+        display.textContent = 0;  
+        currentNum = display.textContent;
+        firstNum = currentNum;
+    } else if (value === '.' && display.textContent.includes('.')) return
+        
+   if (firstNum.length < 15){
+        if (firstOperand == '' && secondNum == ''){
+            firstNum = firstNum + value;
+            currentNum = firstNum;
+        }
     }
     
-    
-
-    if (operand != '' && firstNum != '') {
-        secondNum = secondNum + value;
-        currentNum = secondNum;
+    if (secondNum.length < 15){
+        if (firstOperand != '' && firstNum != null) {
+            secondNum = secondNum + value;
+            currentNum = secondNum;
+        }
     }
-    
- 
-    
-
-
 }
 
-
-
-
+//function to display the value on the screen
 function displayValue () {    
     display.textContent = currentNum;
-
-
-
     }
 
-function selectOperator(value) {
-    
-    operand = value;
-    
-    if (operand != '' && firstNum != '' && secondNum != ''){
-        toEqual(value);
-    }
-        
-    
-
+//function to select operator
+function selectOperator(value) {  
+    if (firstOperand != '' && firstNum != '' && secondNum != ''){
+        toEqual();
+    }    
+    firstOperand = value;  
 }
-//addeventlisteners for each button clicked
-//calls displayvalue 
 
+//function to change the sign to positive or negative
+function changeSign() {
+      if (display.textContent != 0 && firstNum == currentNum) {
+        firstNum = firstNum *-1;
+        currentNum = firstNum;
+        display.textContent = firstNum;
+    } else if (display.textContent != 0 && secondNum == currentNum){
+        secondNum = secondNum *-1;
+        currentNum = secondNum;
+        display.textContent = secondNum;
+    }
+}
+  
+//function to delete on number at a time.
+function toDelete() {    
+    if (display.textContent != 0 && firstNum == currentNum) {
+        firstNum = firstNum.toString().slice(0,-1);
+        currentNum = firstNum;
+        display.textContent = firstNum;
+    } else if (display.textContent != 0 && secondNum == currentNum){
+        secondNum = secondNum.toString().slice(0,-1);
+        currentNum = secondNum;
+        display.textContent = secondNum;
+    }
+}
+    
+//evemtlistners for all the digits
 const numbers = document.querySelectorAll('.digits');
     numbers.forEach((number) => {
         number.addEventListener('click', () => {
@@ -146,15 +161,18 @@ operators.forEach((operate) => {
     });
 });
 
+//addeventlistener for the equal sign
 const equal = document.querySelector('.equal');
 equal.addEventListener('click', () => toEqual());
 
-// --------------------// --------------------// --------------------// --------------------// --------------------
-
-
-
+//addeventlistener for clear
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', () => toClear());
 
-// const decimal = document.querySelector('.decimal');
-// decimal.addEventListener('click', () => selectNumber(decimal.textContent));
+//addeventlistener for the postive and negative button
+const positiveNegative = document.querySelector('.add-minus');
+positiveNegative.addEventListener('click', () => changeSign());
+
+//addeventlistener for the backspace button
+const backSpace = document.querySelector('.back-space');
+backSpace.addEventListener('click', () => toDelete());
